27e00715328c6c24136d604caf9391e4f0e2cbee
akram-cloudly <akram@cloudly.io>
2024-09-18 16:33:40 +0600

    Created a directory called "modular" and placed related files

---
+++ mplane_server/modular/inc/test_wrapper.h
@@ -0,0 +1,23 @@
+#ifndef TEST_WRAPPER_H_
+#define TEST_WRAPPER_H_
+
+#include "MplaneInterfaces.h"
+
+void test_MplaneInterfaces();
+void test_MplaneProcessingElement();
+void test_MplaneUplaneConf();
+void test_MplaneAlarms();
+void test_MplaneEcpri();
+void test_MplaneExternalio();
+void test_MplanePerformanceMgmt();
+void test_MplaneHardwae();
+void test_MplaneFan();
+void test_MplaneAntennaCalibration();
+void test_MplaneAld();
+void test_MplaneBeamforming();
+void test_MplaneSync();
+void test_MplaneDelayMgmt();
+void test_MplaneModuleCapability();
+void test_MplaneSupervision();
+
+#endif /* TEST_WRAPPER_H_ */
---
+++ mplane_server/modular/src/hal_loader.cpp
@@ -0,0 +1,69 @@
+#include "hal_loader.h"
+#include <cstddef>
+#include <iostream> 
+#include "wrapper.h"
+
+Hal_Loader::Hal_Loader(const char* _libname)
+{
+    libname = _libname;
+  if(libname)
+    {
+      dlhandle = dlopen("/usr/lib/libhalmplane.so.0.1.0", RTLD_NOW | RTLD_GLOBAL);
+    
+      if (!dlhandle) 
+      {
+          std::cerr << dlerror() << std::endl;
+        //   return ;
+      }
+      void* (*rf)() = reinterpret_cast<void* (*)()>(dlsym(dlhandle, "function_map"));
+      if(rf)
+      {
+        // rf();
+        // std::cout <<"success full" << std::endl;
+        registered_functions = (std::map<std::string, void*>*) rf();
+      }
+      else
+      {
+        registered_functions = new std::map<std::string, void*>();
+      }
+    }
+  else
+    {
+      registered_functions = new std::map<std::string, void*>();
+    }
+
+    std::cout <<"hal_loader.cpp" << std::endl;
+    // halmplane_error_t halmplane_interface_update(interface_t* interface);
+
+    interface_t* xyz;
+    halmplane_error_t (*halmplane_interface_update)(interface_t*) = (halmplane_error_t (*)(interface_t*)) dlsym(dlhandle,"halmplane_interface_update");
+    // std::cout <<"address of int (*halmplane_init)() hal-loader" << (*registered_functions)["int (*halmplane_init)()"] << std::endl;
+
+    if(halmplane_interface_update)
+    {
+      std::cout << "successfull of hal_loader.cpp" << std::endl;
+      halmplane_interface_update(xyz);
+    }
+
+}
+
+void* Hal_Loader::get_function(const char* ftag)
+{
+  if(registered_functions)
+    {
+      return (*registered_functions)[ftag];
+    }
+  return NULL;
+}
+
+void Hal_Loader::Hal_close()
+{
+  if(dlclose(dlhandle) == 0)
+  {
+    std::cout << "Library has closed" << std::endl;
+  }
+  else 
+  {
+    std::cout << "Library has not closed" << std::endl;
+  }
+}
